89bb6842e4d6f4b795f53d703991a9f81b769d43
Anthony Simpson <ansi6622@colorado.edu>
2016-05-18 00:48:01 -0600

    initial commit, this repo is for recursion currently, but was originally, and will probably head back towards a focus on the euler stuff

---
+++ app.js
@@ -0,0 +1,21 @@
+function Fibonacci() {
+
+  this.looping = function(n){
+    var a = 0, b = 1, f =1;
+    for (var i = 2; i <= n; i++) {
+      f = a + b;
+      a = b;
+      b = f;
+    }
+    return f;
+  }
+
+  this.recursive = function(n){
+    if(n <= 2){
+      return 1;
+    }
+    else {
+      return this.recursive(n-1) + this.recursive(n - 2)
+    }
+  }
+}
